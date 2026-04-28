6a85dd7f25a51512afabf35ea5c674f12043aafd
Dave Roberts <d.roberts@ctrlo.com>
2026-04-28 16:47:16 +0100

    Initial version complete

---
+++ readme.md
@@ -0,0 +1,29 @@
+# @ctrlo/Common
+
+Common module used in some CtrlO libraries
+
+## functions
+
+```javascript
+hideElement(el);
+```
+
+Hides the element defined in the variable `el` - this has the advantage over JQuery's `hide()` function as it also adds `aria-hidden` to the element
+
+```javascript
+showElement(el);
+```
+
+Shows the element defined the variable `el` - this has the advantage over JQuery's `show()` function as it also removes the `aria-hidden` attribute from the element should it be present
+
+```javascript
+fromJson(item)
+```
+
+Converts an item from a string to an object - when the input is unknown (i.e. may be an object or a string) and an object is passed in the function returns the original object. Should there be an error in parsing the object, it will return an empty object.
+
+```javascript
+coerceError(item)
+```
+
+This function can receive a string or an Error object and is designed for when it is unknown which will be received. It will return the error message, or the original string object, if one is passed in.
---
+++ src/common.test.ts
@@ -0,0 +1,92 @@
+import { fromJson, hideElement, showElement } from './common';
+import { describe, it, expect, beforeEach, afterEach, jest } from '@jest/globals';
+
+describe('common functions', () => {
+    describe('CSS and ARIA - skipped as they are incorrect', () => {
+        let el: JQuery<HTMLElement>;
+
+        beforeEach(() => {
+            el = $(document.createElement('div'));
+        });
+
+        afterEach(() => {
+            jest.clearAllMocks();
+        });
+
+        it('hides an element', () => {
+            const hasClass = jest.spyOn(el, 'hasClass');
+            const addClass = jest.spyOn(el, 'addClass');
+            const attr = jest.spyOn(el, 'attr');
+            hideElement(el);
+            expect(hasClass).toHaveBeenCalledWith('hidden');
+            expect(addClass).toHaveBeenCalledWith('hidden');
+            expect(attr).toHaveBeenCalledWith('aria-hidden', 'true');
+        });
+
+        it('does not hide a hidden element', () => {
+            el.addClass('hidden');
+            const hasClass = jest.spyOn(el, 'hasClass');
+            const addClass = jest.spyOn(el, 'addClass');
+            const attr = jest.spyOn(el, 'attr');
+            hideElement(el);
+            expect(hasClass).toHaveBeenCalledWith('hidden');
+            expect(addClass).not.toHaveBeenCalled();
+            expect(attr).not.toHaveBeenCalled();
+        });
+
+        it('shows a hidden element', () => {
+            el.addClass('hidden');
+            const hasClass = jest.spyOn(el, 'hasClass');
+            const removeClass = jest.spyOn(el, 'removeClass');
+            const removeAttr = jest.spyOn(el, 'removeAttr');
+            showElement(el);
+            expect(hasClass).toHaveBeenCalledWith('hidden');
+            expect(removeClass).toHaveBeenCalledWith('hidden');
+            expect(removeAttr).toHaveBeenCalledWith('aria-hidden');
+        });
+
+        it('does not show a visible element', () => {
+            const hasClass = jest.spyOn(el, 'hasClass');
+            const removeClass = jest.spyOn(el, 'removeClass');
+            const removeAttr = jest.spyOn(el, 'removeAttr');
+            showElement(el);
+            expect(hasClass).toHaveBeenCalledWith('hidden');
+            expect(removeClass).not.toHaveBeenCalled();
+            expect(removeAttr).not.toHaveBeenCalled();
+        });
+    });
+
+    describe('JSON tests', () => {
+        it('parses a JSON string', () => {
+            const json = '{"foo":"bar"}';
+            const parsed = fromJson(json);
+            expect(parsed.foo).toEqual('bar');
+        });
+
+        it('parses a JSON object', () => {
+            const json = { foo: 'bar' };
+            const parsed = fromJson(json);
+            expect(parsed.foo).toEqual('bar');
+        });
+
+        it('returns an empty object for invalid JSON', () => {
+            const json = 'foo';
+            const parsed = fromJson(json);
+            expect(parsed).toEqual({});
+        });
+
+        it('returns an empty object for null', () => {
+            const json = null;
+            /* @ts-ignore */
+            const parsed = fromJson(json);
+            expect(parsed).toEqual({});
+        });
+
+        it('returns an empty object for undefined', () => {
+            const json = undefined;
+            /* @ts-ignore */
+            const parsed = fromJson(json);
+            expect(parsed).toEqual({});
+        });
+    });
+});
